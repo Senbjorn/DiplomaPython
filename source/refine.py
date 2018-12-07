f57c290bb31631b9bb37bc31c940a02f30cfd47a
semyon <glushkov.sm@phystech.edu>
2018-12-07 06:19:04 +0300

    Refine

--- source/refine.py
+++ source/refine.py
@@ -10,12 +10,41 @@ from simtk.unit import *
 import prody as pdy
 
 
-"""
-Creates system as .pdb file containing appropriate configuration for DRSystem.
-:param path: path to .pdb file with initial system.
-:param tmp_file: is a file containing new system.
-"""
+def rotation_matrix(alpha, beta, gamma):
+    """
+    Creates rotation matrix.
+    @param alpha: rotation angle about x axis.
+    @param beta: rotation angle about y axis.
+    @param gamma: rotation angle about z axis.
+    @return: 3x3 rotation matrix.
+    @rtype: numpy.ndarray
+    """
+    rx = np.array([
+        [1,             0,              0],
+        [0, np.cos(alpha), -np.sin(alpha)],
+        [0, np.sin(alpha),  np.cos(alpha)],
+    ])
+    ry = np.array([
+        [ np.cos(beta), 0, np.sin(beta)],
+        [            0, 1,            0],
+        [-np.sin(beta), 0, np.cos(beta)],
+    ])
+    rz = np.array([
+        [np.cos(gamma), -np.sin(gamma), 0],
+        [np.sin(gamma),  np.cos(gamma), 0],
+        [            0,              0, 1],
+    ])
+    return rx.dot(ry).dot(rz)
+
+
 def create_system(path, tmp_file="../output/tmp_system.pdb"):
+    """
+    Creates system as .pdb file containing appropriate configuration for DRSystem.
+    @param path:  path to .pdb file with initial system.
+    @type path: str
+    @param tmp_file: a file containing new system.
+    @type tmp_file: str
+    """
     pdy_protein = pdy.parsePDB(path)
     pdy_protein = pdy_protein.select("protein")
     time0 = time.time()
@@ -34,34 +63,35 @@ def create_system(path, tmp_file="../output/tmp_system.pdb"):
     print("write PDB(openmm):", time.time() - time0, "sec")
 
 
-"""
-TODO: save calculated energy and force for speed up purposes.
-"""
+# TODO: save calculated energy and force for speed up purposes.
 class DRSystem:
+    # PDY
     _pdy_protein_init = None
-    _omm_protein = None
+    _ligand_init = None
     _ligand = None
-    _substrate = None
+    _substrate_init = None
+    # OMM
+    _omm_protein = None
     _simulation = None
-    # _modes_init = None
-    # _modes = None
-    # _eigenvalues = None
-    # _anm = None
+    _center = None
 
-    """
-    Creates a new system for docking refinement.
-    :param pdb_file: path to a file containing system from which different samples are produced.
-    Chain A is a ligand to refine. Chain B is a substrate. Note that it should meet all forcefield requirements such as
-    hydrogens etc.
-    :param forcefiled_name: name of forcefiled which is necessary to calculate force vector and energy
-    :type pdb_file: str
-    :type forcefiled: str
-    """
-    def __init__(self, pdb_file, forcefield_name, n_modes=10):
+    def __init__(self, pdb_file, forcefield_name):
+        """
+        Creates a new system for docking refinement.
+        @param pdb_file: path to a file containing system from which different samples are produced.
+        Chain A is a ligand to refine. Chain B is a substrate. Note that it should meet
+        all forcefield requirements such as hydrogens etc.
+        @type pdb_file: str
+        @param forcefield_name: name of forcefiled which is necessary to calculate force vector and energy.
+        @type forcefield_name: str
+        """
         self._pdy_protein_init = pdy.parsePDB(pdb_file)
         self._omm_protein = app.PDBFile(pdb_file)
-        self._ligand = self._pdy_protein_init.select("chain A")
-        self._substrate = self._pdy_protein_init.select("chain B")
+        self._ligand = self._pdy_protein_init.select("chain A").copy()
+        self._ligand_init = self._pdy_protein_init.select("chain A")
+        self._substrate_init = self._pdy_protein_init.select("chain B")
+        self._center = pdy.calcCenter(self._ligand, weights=self._ligand.getMasses())
+        self._center_init = pdy.calcCenter(self._ligand_init, weights=self._ligand_init.getMasses())
 
         # OpenMM System
         forcefield = app.ForceField(forcefield_name)
@@ -73,18 +103,29 @@ class DRSystem:
         self._simulation = app.Simulation(self._omm_protein.topology, system, integrator)
         self._simulation.context.setPositions(self._omm_protein.positions)
 
-    """
-    Returns current position.
-    :returns: position of the ligand as an array of 3d coordinates.
-    """
+    def get_init_position(self):
+        """
+        Returns initial position.
+        @return: initial position of the ligand as an array of 3d coordinates in angstrom.
+        @rtype: numpy.ndarray
+        """
+        return self._ligand_init.getCoords()
+
     def get_position(self):
+        """
+        Returns current position.
+        @return: position of the ligand as an array of 3d coordinates in angstrom.
+        @rtype: numpy.ndarray
+        """
         return self._ligand.getCoords()
 
-    """
-    Updates ligand position.
-    :param new_position: new ligand position in angstrom
-    """
+
     def set_position(self, new_position):
+        """
+        Updates ligand position.
+        @param new_position: new ligand position in angstrom.
+        @type new_position: numpy.ndarray
+        """
         self._ligand.setCoords(new_position)
         iterator = self._ligand.iterAtoms()
         i = 0
@@ -93,42 +134,55 @@ class DRSystem:
             i += 1
         self._simulation.context.setPositions(self._omm_protein.positions)
 
-    """
-    Get ligand.
-    :returns: ligand object.
-    """
     def get_ligand(self):
+        """
+        Get ligand.
+        @return: ligand object.
+        @rtype: ProDy.AtomGroup
+        """
         return self._ligand
 
-    """
-    Calculates energy of the whole system.
-    :returns: energy value in kDJ/mol
-    """
+
     def get_energy(self):
+        """
+       Calculates energy of the whole system.
+       @return: energy value in kDJ/mol.
+       @rtype: float
+       """
         state = self._simulation.context.getState(getEnergy=True)
         return state.getPotentialEnergy().value_in_unit(kilojoule_per_mole)
 
-    """
-    Calculates force vector that acts upon ligand.
-    :returns: force vector
-    """
     def get_force(self):
+        """
+       Calculates force vector that acts upon ligand.
+       @return: force vector in kJ/mole/nm.
+       @rtype: numpy.ndarray
+       """
         state = self._simulation.context.getState(getForces=True)
-        return state.getForces()
+        return np.array(state.getForces().value_in_unit(kilojoule_per_mole / nanometer))
 
-    """
-    Translates initial system on vector t and rotates via rotation operator r.
-    :param t: translation vector t.
-    :param r: rotation operator.
-    """
+
     def set_rigid(self, t, r):
-        pass
-
+        """
+        Translates initial system by vector t and rotates via rotation operator r.
+        @param t: translation vector t.
+        @type t: np.ndarray
+        @param r: rotation operator.
+        @type r: np.ndarray
+        """
+        pos = self.get_init_position().copy()
+        self._center = self._center_init + t
+        for i in range(len(pos)):
+            pos[i] += t
+            pos[i] = np.dot(r, pos[i] - self._center) + self._center
+        self.set_position(pos)
     # """
     # Selects a part of the system.
     # """
     # def select(self, selstr, **kwargs):
     #     self._ligand = self._ligand.select(selstr, **kwargs)
+
+
 class NMSpaceWrapper:
     _system = None
     _modes_init = None
@@ -137,151 +191,89 @@ class NMSpaceWrapper:
     _anm = None
     _position = None
 
-    """
-    Create new wrapper of DRSystem instance. It allows you to operate the system in NM space.
-    :param drs: DRSystem instance to wrap.
-    :param n_modes: number of modes. Should be less than NumberOfAtoms - 6.
-    :type drs: DRSystem.
-    :type n_modes: int.
-    """
-    def __init__(self, drs, n_modes = 10):
+    def __init__(self, drs, n_modes=10):
+        """
+        Create new wrapper of DRSystem instance. It allows you to operate the system in NM space.
+        @param drs: DRSystem instance to wrap.
+        @type drs: DRSystem
+        @param n_modes: number of modes. Should be less than NumberOfAtoms - 6.
+        @type n_modes: int
+        """
         self._position = np.zeros(n_modes)
         self._system = drs
-        # TODO: add normal modes
         self._anm = pdy.ANM('anm')
         self._anm.buildHessian(self._system.get_ligand())
         self._anm.calcModes(n_modes=n_modes, zeros=False)
-        self._modes_init = self._anm.getEigvecs()
-        self._modes = self._anm.getEigvecs()
+        self._modes_init = self._anm.getEigvecs().copy().T
+        self._modes = self._anm.getEigvecs().copy().T
         self._eigenvalues = self._anm.getEigvals()
 
-    """
-    Returns current position in NM space.
-    :returns: position of the ligand as an m-d vector from NM space.
-    """
     def get_position(self):
+        """
+        Returns current position in NM space.
+        @return: position of the ligand as an m-d vector from NM space.
+        @rtype: numpy.ndarray
+        """
         return self._position
 
-    """
-    Updates ligand position in NM space.
-    :param new_position: new ligand position in NM space.
-    """
     def set_position(self, new_position):
-        real_pos = self._system.get
+        """
+        Updates ligand position in NM space.
+        @param new_position: new ligand position in NM space.
+        @type new_position: numpy.ndarray
+        """
+        self._position = new_position
+        atom_position = self._system.get_position() + np.dot(self._modes, self._position)
+        self._system.set_position(atom_position)
 
-    """
-    Calculates energy of the whole system.
-    :returns: energy value in kDJ/mol.
-    """
     def get_energy(self):
+        """
+        Calculates energy of the whole system.
+        @return: energy value in kDJ/mol.
+        @rtype: float
+        """
         return self._system.get_energy()
 
-    """
-    Calculates force vector that acts upon each mode.
-    :returns: force vector in NM space.
-    """
     def get_force(self):
+        """
+        Calculates force vector that acts upon each mode.
+        @return: force vector in NM space.
+        @rtype: numpy.ndarray
+        """
         aw_force = self._system.get_force()
-        # TODO: calculate modewise force
+        force_1d = np.reshape(aw_force, (aw_force.shape[0] * 3,))
+        return np.dot(self._modes, force_1d)
 
-    """
-    Translates initial system on vector t and rotates via rotation operator r.
-    :param t: translation vector t.
-    :param r: rotation operator.
-    """
     def set_rigid(self, t, r):
+        """
+        Translates initial system by vector t and rotates via rotation operator r.
+        @param t: translation vector t.
+        @type t: np.ndarray
+        @param r: rotation operator.
+        @type r: np.ndarray
+        """
         self._system.set_rigid(t, r)
-        # TODO: move modes (rotate)
+        m = self._modes_init.shape[0]
+        n = self._modes_init.shape[1]
+        print(m)
+        tmp_modes = np.reshape(self._modes_init, (m, n // 3, 3)).copy()
+        for i in range(m):
+            for j in range(n // 3):
+                tmp_modes[i][j] = np.dot(r, tmp_modes[i][j])
+        self._modes = np.reshape(tmp_modes, (m, n))
 
-    """
-    Get normal modes.'
-    :returns: normal modes.
-    """
     def get_modes(self):
+        """
+        Get normal modes.'
+        @return: normal modes (shape = (m, 3N)).
+        @rtype: numpy.ndarray
+        """
         return self._modes
 
-    """
-    Get eigenvalues.
-    :returns: eigenvalues.
-    """
     def get_eigenvalues(self):
+        """
+        Get eigenvalues.
+        @return: eigenvalues.
+        @rtype: numpy.ndarray
+        """
         return self._eigenvalues
-
-
-# Unit
-def Test_1():
-    print("START TEST 1")
-    path = "../data/2cds.pdb"
-    create_system(path)
-    t0 = time.time()
-    drs = DRSystem("../output/tmp_system.pdb", 'charmm36.xml')
-    print("construction of system:", -t0 + time.time(), "sec")
-    t0 = time.time()
-    s = 0
-    for i in range(100):
-        s = drs.get_energy()
-    print("energy time repeat:", (-t0 + time.time()) / 100., "sec")
-    t0 = time.time()
-    s = 0
-    for i in range(100):
-        s = drs.get_force()
-    print("force time:", (-t0 + time.time()) / 100., "sec")
-    print("OK")
-    print("END TEST 1")
-
-
-def Test_2():
-    print("START TEST 2")
-    path = "../data/2cds.pdb"
-    create_system(path)
-    t0 = time.time()
-    drs = DRSystem("../output/tmp_system.pdb", 'charmm36.xml')
-    print("construction of system:", -t0 + time.time(), "sec")
-    t0 = time.time()
-    s = 0
-    time_full = 0
-    coordinates = drs.get_position()
-    for i in range(100):
-        offset = np.random.random(coordinates.shape) / 5
-        t0 = time.time()
-        drs.set_position(coordinates + offset)
-        time_full += time.time() - t0
-    print("set_position time:", time_full / 100., "sec")
-
-    print("OK")
-    print("END TEST 2")
-
-
-def Test_3():
-    print("START TEST 3")
-    path = "../data/2cds.pdb"
-    create_system(path)
-    t0 = time.time()
-    drs = DRSystem("../output/tmp_system.pdb", 'charmm36.xml')
-    print("construction of system:", -t0 + time.time(), "sec")
-    t0 = time.time()
-    coordinates = drs.get_position()
-    time_full = 0
-    value = None
-    for i in range(100):
-        offset = np.random.random(coordinates.shape) / 5
-        drs.set_position(coordinates + offset)
-        t0 = time.time()
-        value = drs.get_energy()
-        time_full += time.time() - t0
-    print("energy time:", time_full / 100., "sec")
-    time_full = 0
-    value = None
-    for i in range(100):
-        offset = np.random.random(coordinates.shape) / 5
-        drs.set_position(coordinates + offset)
-        t0 = time.time()
-        value = drs.get_force()
-        time_full += time.time() - t0
-    print("force time:", (time_full) / 100., "sec")
-    print("OK")
-    print("END TEST 3")
-
-
-if __name__ == "__main__":
-    Test_1()
